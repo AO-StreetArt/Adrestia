276fee439f08a414425a93012eba24b22c75b872
Alex Barry <aostreetart9@gmail.com>
2018-04-15 01:48:07 -0400

    remove unnecessary clyman configurations from populate consul script

--- scripts/linux/populate_consul.sh
+++ scripts/linux/populate_consul.sh
@@ -13,12 +13,10 @@ $CURL_COMMAND -X PUT -d 'neo4j://graph-db:7687' http://$CONSUL_HOST:8500/v1/kv/i
 $CURL_COMMAND -X PUT -d 'True' http://$CONSUL_HOST:8500/v1/kv/ivan/StampTransactionId
 $CURL_COMMAND -X PUT -d 'True' http://$CONSUL_HOST:8500/v1/kv/ivan/AtomicTransactions
 $CURL_COMMAND -X PUT -d 'Json' http://$CONSUL_HOST:8500/v1/kv/ivan/Data_Format_Type
-$CURL_COMMAND -X PUT -d 'cache--6379----2--5--0' http://$CONSUL_HOST:8500/v1/kv/clyman/RedisConnectionString
 $CURL_COMMAND -X PUT -d 'mongodb://document-db:27017/' http://$CONSUL_HOST:8500/v1/kv/clyman/Mongo_ConnectionString
 $CURL_COMMAND -X PUT -d 'mydb' http://$CONSUL_HOST:8500/v1/kv/clyman/Mongo_DbName
 $CURL_COMMAND -X PUT -d 'test' http://$CONSUL_HOST:8500/v1/kv/clyman/Mongo_DbCollection
 $CURL_COMMAND -X PUT -d 'True' http://$CONSUL_HOST:8500/v1/kv/clyman/StampTransactionId
-$CURL_COMMAND -X PUT -d 'False' http://$CONSUL_HOST:8500/v1/kv/clyman/AtomicTransactions
 $CURL_COMMAND -X PUT -d 'True' http://$CONSUL_HOST:8500/v1/kv/clyman/ObjectLockingActive
 $CURL_COMMAND -X PUT -d 'JSON' http://$CONSUL_HOST:8500/v1/kv/clyman/DataFormatType
 $CURL_COMMAND -X PUT -d 'queue:9092' http://$CONSUL_HOST:8500/v1/kv/clyman/KafkaBrokerAddress
