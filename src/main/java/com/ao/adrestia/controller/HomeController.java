35a9087ae23b9e8bd1b8b811fce8619fa05d74ce
Alex Barry <aostreetart9@gmail.com>
2018-11-27 21:01:54 -0500

    Allow access to the homepage if authorization is inactive

--- src/main/java/com/ao/adrestia/controller/HomeController.java
+++ src/main/java/com/ao/adrestia/controller/HomeController.java
@@ -23,6 +23,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -31,12 +32,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 
+  @Value(value = "${server.auth.active}")
+  private boolean authActive;
+
   private final Logger logger = LoggerFactory.getLogger("adrestia.AuthController");
 
   @RequestMapping(value = "/portal/home", method = RequestMethod.GET)
   protected String home(final Map<String, Object> model, final Principal principal) {
     logger.info("Home page");
-    if (principal == null) {
+    if (principal == null && authActive) {
       logger.warn("No Principal Detected");
       return "redirect:/logout";
     }
